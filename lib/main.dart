0030d0ac113d7dbb9af6fd0f1486981ff94870af
windsscoreyou-droid <windsscoreyou@gmail.com>
2026-09-08 00:26:34 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
