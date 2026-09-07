f1b53cda1d20225cdd682c15de127b42a9dc6381
windsscoreyou-droid <windsscoreyou@gmail.com>
2026-09-08 00:15:08 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://mobmajamwnxqtrwrsih.supabase.co',
+    url: 'https://mobmajamwnxqtrwrsihj.supabase.co',
     publishableKey: 'sb_publishable_MPCFZfn-qNieoL1kBNeFrg_fjJlSWEE',
   );
 
